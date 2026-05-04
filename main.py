7a0628127c03e9bf7aedab08c99baf381ad76551
wera432 <weronika.bartnicka@onet.eu>
2026-05-04 08:44:29 +0200

    slight modification

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import pint 
+import pint
 import numpy as np
 from scipy import constants
 
 si = pint.UnitRegistry()
 
+
 def equilibrium_vapor_pressure(T):
     TC = T - constants.zero_Celsius * si.K
-    return .61078 * si.kPa * np.exp(17.27 * TC / (TC + 237.3 * si.K))
+    return 0.61078 * si.kPa * np.exp(17.27 * TC / (TC + 237.3 * si.K))
 
-print(f"{equilibrium_vapor_pressure(T= 300 * si.K)=:.2g~}")
+
+print(f"{equilibrium_vapor_pressure(T= 273 * si.K)=:.2g~}")
