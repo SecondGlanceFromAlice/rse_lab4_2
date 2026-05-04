52fda8e559157aa2d186591f8fe935a055d3e74a
wera432 <weronika.bartnicka@onet.eu>
2026-05-04 08:21:18 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pint 
+import numpy as np
+from scipy import constants
+
+si = pint.UnitRegistry()
+
+def equilibrium_vapor_pressure(T):
+    TC = T - constants.zero_Celsius * si.K
+    return .61078 * si.kPa * np.exp(17.27 * TC / (TC + 237.3 * si.K))
+
+print(f"{equilibrium_vapor_pressure(T= 300 * si.K)=:.2g~}")
