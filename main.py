bf6e1e4ad97611f1518cb8f6eaf23fc8535ec98c
Alicja <ajagielska@com-4t.pl>
2026-05-04 09:06:39 +0200

    Added main()

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import pint
 import numpy as np
 from scipy import constants
 
-si = pint.UnitRegistry()
-
-
 def equilibrium_vapor_pressure(temperature):
     tc = temperature - constants.zero_Celsius * si.K
     return 0.61078 * si.kPa * np.exp(17.27 * tc / (tc + 237.3 * si.K))
 
+def main():
+    si = pint.UnitRegistry()
 
-print(f"{equilibrium_vapor_pressure(temperature= 273 * si.K)=:.2g~}")
+    print(f"{equilibrium_vapor_pressure(temperature= 273 * si.K)=:.2g~}")
