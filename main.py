d36ad57ab06b7712510dcbd6153bead1e89772b7
Alicja <ajagielska@com-4t.pl>
2026-05-04 09:05:13 +0200

    Fix some pylint errors

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from scipy import constants
 si = pint.UnitRegistry()
 
 
-def equilibrium_vapor_pressure(T):
-    TC = T - constants.zero_Celsius * si.K
-    return 0.61078 * si.kPa * np.exp(17.27 * TC / (TC + 237.3 * si.K))
+def equilibrium_vapor_pressure(temperature):
+    tc = temperature - constants.zero_Celsius * si.K
+    return 0.61078 * si.kPa * np.exp(17.27 * tc / (tc + 237.3 * si.K))
 
 
-print(f"{equilibrium_vapor_pressure(T= 273 * si.K)=:.2g~}")
+print(f"{equilibrium_vapor_pressure(temperature= 273 * si.K)=:.2g~}")
